35b262861bf97b6cdef7280999405c0816c4da73
armena <armen.arzumanyan@optym.com>
2019-03-12 17:11:22 +0400

    adding util classes

---
+++ investor/src/main/java/com/front/web/util/ParamUtil.java
@@ -0,0 +1,95 @@
+
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+package com.front.web.util;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Armen Arzumanyan
+ */
+public class ParamUtil {
+    static public Double doubleValue(String strValue) {
+        Double reValue = null;
+
+        if ((strValue == null) || (strValue.trim().equals(""))) {
+            strValue = null;
+        } else if (strValue == null) {
+            return null;
+        }
+
+        DecimalFormatSymbols fs = new DecimalFormatSymbols();
+
+        fs.setGroupingSeparator(',');
+        fs.setDecimalSeparator('.');
+
+        try {
+            DecimalFormat nf = new DecimalFormat("#,###,###,##0.00", fs);
+
+            nf.setMaximumFractionDigits(3);
+            nf.setMaximumIntegerDigits(3);
+            reValue = nf.parse(strValue).doubleValue();
+        } catch (final ParseException e) {}
+
+        return reValue;
+    }
+
+    static public Long longValue(String strValue) {
+        Long reValue = null;
+
+        if ((strValue == null) || (strValue.trim().equals(""))) {
+            strValue = null;
+        } else if (strValue == null) {
+            return null;
+        }
+
+        NumberFormat nf = NumberFormat.getInstance();
+
+        try {
+            reValue = (Long) nf.parse(strValue).longValue();
+        } catch (ParseException ex) {}
+
+        return reValue;
+    }
+
+    static public Integer integerValue(Object strValue) {
+        return integerValue((strValue != null)
+                            ? strValue.toString()
+                            : null);
+    }
+
+    static public Long longValue(Object strValue) {
+        return longValue((strValue != null)
+                         ? strValue.toString()
+                         : null);
+    }
+
+    static public Integer integerValue(String strValue) {
+        Integer reValue = null;
+
+        if ((strValue == null) || (strValue.trim().equals(""))) {
+            strValue = null;
+        } else if (strValue == null) {
+            return null;
+        }
+
+        NumberFormat nf = NumberFormat.getInstance();
+
+        try {
+            reValue = (Integer) nf.parse(strValue).intValue();
+        } catch (ParseException ex) {}
+
+        return reValue;
+    }
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
